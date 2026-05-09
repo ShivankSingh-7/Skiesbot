db4a4f1e83b1f788f5e70385979805d674fdbec3
Shivank Singh <shivangsingh819@gmail.com>
2026-05-09 10:10:27 +0530

    created a simple chatbot

---
+++ Backend.py
@@ -0,0 +1,41 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+os.environ['GROQ_API_KEY'] = os.getenv('GROQ_API_KEY')
+
+from langchain_groq import ChatGroq
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.graph.message import add_messages
+from langchain_core.messages import BaseMessage, HumanMessage
+
+llm = ChatGroq(
+    model="llama-3.3-70b-versatile"
+)
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+    
+def chat_node(state: ChatState):
+    messages = state['messages']
+    
+    response = llm.invoke(messages)
+    return {"messages": [response]}
+
+#checkpointer
+checkpointer = InMemorySaver()
+
+#graph
+graph = StateGraph(ChatState)
+
+#add node
+graph.add_node('chat_node', chat_node)
+
+#add edges
+graph.add_edge(START, 'chat_node')
+graph.add_edge('chat_node', END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
+    
