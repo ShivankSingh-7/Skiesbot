da6a3f051fe267cf293f8057eb804006f2b8532a
Shivank Singh <shivangsingh819@gmail.com>
2026-05-10 18:20:24 +0530

    created a database using sqlite so our data get stored permanently

--- langgraph_database.py
+++ langgraph_database.py
@@ -1,26 +1,20 @@
-import os
+import os 
 from dotenv import load_dotenv
 load_dotenv()
 
-groq_api_key = os.getenv("GROQ_API_KEY")
-
-print(groq_api_key)  # debug
-
-if not groq_api_key:
-    raise ValueError("GROQ_API_KEY not found")
+os.environ['GROQ_API_KEY'] = os.getenv('GROQ_API_KEY')
 
 from langchain_groq import ChatGroq
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
-from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph.message import add_messages
 from langchain_core.messages import BaseMessage, HumanMessage
+from langgraph.checkpoint.sqlite import SqliteSaver
+import sqlite3
 
 llm = ChatGroq(
-    model="llama-3.3-70b-versatile",
+    model="llama-3.3-70b-versatile"
 )
-
-print(llm)
 
 class ChatState(TypedDict):
     messages: Annotated[list[BaseMessage], add_messages]
@@ -31,8 +25,11 @@ def chat_node(state: ChatState):
     response = llm.invoke(messages)
     return {"messages": [response]}
 
+# sqlite data 
+con = sqlite3.connect(database='chatbot.db', check_same_thread=False)
+
 #checkpointer
-checkpointer = InMemorySaver()
+checkpointer = SqliteSaver(conn = con)
 
 #graph
 graph = StateGraph(ChatState)
@@ -46,3 +43,13 @@ graph.add_edge('chat_node', END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
 
+# how to get number of threads
+
+
+def retrieve_threads():
+    all_threads = set()
+    
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
+        
+    return list(all_threads)
---
+++ resume_frontend.py
@@ -0,0 +1,102 @@
+import streamlit as st
+from Backend import chatbot
+from langchain_core.messages import HumanMessage
+import uuid
+
+#*********************** utility function ***************************
+def generate_thread_id():
+    thread_id = uuid.uuid4()
+    return thread_id
+
+def reset_chat():
+    thread_id = generate_thread_id()
+    st.session_state['thread_id'] = thread_id
+    add_thread(st.session_state['thread_id'])
+    st.session_state['message_history'] = []
+    
+def add_thread(thread_id):
+    if thread_id not in st.session_state['chat_thread']:
+        st.session_state['chat_thread'].append(thread_id)
+        
+def old_chat(thread_id):
+    old_message = chatbot.get_state(config={'configurable': {'thread_id' : thread_id}}).values['messages']
+    
+    if old_message:
+        return old_message
+    else:
+        return {}
+        
+    
+
+# ****************************** setup ***************************************
+
+if 'thread_id' not in st.session_state:
+    st.session_state['thread_id'] = generate_thread_id()
+
+CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
+
+
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+    
+if 'chat_thread' not in st.session_state:
+    st.session_state['chat_thread'] = []
+    
+add_thread(st.session_state['thread_id'])
+
+# *****************************SIDEBAR UI ***********************************************
+st.sidebar.title('LNAGGRAPH CHATBOT')
+
+
+if st.sidebar.button('New Chat'):
+    reset_chat()
+    
+    
+    
+st.sidebar.header('My Conversation')
+
+
+for thread_id in st.session_state['chat_thread']:
+    if st.sidebar.button(str(thread_id)):
+        st.session_state['thread_id'] = thread_id
+        messages = old_chat(thread_id)       
+
+        temp_messages = []
+        
+        for msg in messages:
+            if isinstance(msg, HumanMessage):
+                role = 'user'
+            else:
+                role='assistant'
+            temp_messages.append({'role':role, 'content': msg.content})
+            
+        st.session_state['message_history'] = temp_messages
+
+
+# ******************* functionality ***********************************
+    
+for message in st.session_state['message_history']:
+    
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+        
+
+user_input = st.chat_input('Type here')
+
+if user_input:
+    
+    st.session_state['message_history'].append({'role': 'user', 'content': user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+        
+    with st.chat_message('assistant'):
+        ai_message = st.write_stream(
+            message_chunk for message_chunk, metadata in chatbot.stream(
+                {'messages': [HumanMessage(content=user_input)]},
+                config=CONFIG,
+                stream_mode='messages'
+            )
+        )
+        
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
+    
