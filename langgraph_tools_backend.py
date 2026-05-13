a59c8a1285132ed400c7dcc381afaaf5535b4bad
Shivank Singh <shivangsingh819@gmail.com>
2026-05-13 19:58:42 +0530

    integrated tools like tavily and also make api call to fetch stock data integrated all the tools to our llm model

---
+++ langgraph_tools_backend.py
@@ -0,0 +1,116 @@
+import os 
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_groq import ChatGroq
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage, HumanMessage
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+
+from langgraph.prebuilt import ToolNode, tools_condition
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.tools import tool
+
+from langgraph.checkpoint.sqlite import SqliteSaver
+import sqlite3
+
+import requests
+import random
+
+llm = ChatGroq(
+    model = "llama-3.3-70b-versatile" 
+)
+
+#  search tool
+search_tool = TavilySearchResults(max_result = 3)
+
+@tool
+def calculator(first_num: float, second_num: float, operation: str)-> dict:
+    """
+    Perform a basic arithmetic operation on two numbers.
+    Supported operations: add, sub, mul, div
+    """
+    
+    try:
+        if operation == "add":
+            result = first_num + second_num
+        elif operation == "sub":
+            result = first_num - second_num
+        elif operation == "mul":
+            result = first_num * second_num
+        elif operation == "div":
+            if second_num == 0:
+                return {"error": "Division by zero is not allowed"}
+            result = first_num / second_num
+        else:
+            return {"error": f"unsupported operation '{operation}'"}
+        
+        return {"first_num": first_num, "second_num": second_num, "operation": operation, "result": result}
+    except Exception as e:
+        return {"error": str(e)}
+    
+@tool
+def get_stoc_price(symbol: str) -> dict:
+    """
+    Fetch latest stock price for a given symbol (e.g. 'AAPL', 'TSLA')
+    using Alpha Vantage with API key in the URL
+    """
+    url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey={os.getenv('ALPHA_VANTAGE_API_KEY')}"
+    r = requests.get(url)
+    return r.json()
+
+# Make tool list
+tools = [get_stoc_price, search_tool, calculator]
+
+# make llm aware
+llm_with_tools = llm.bind_tools(tools)
+
+# state
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+    
+def chat_node(state: ChatState):
+    """LLM node that may answer or request a tool call"""
+    messages = state['messages']
+    response = llm_with_tools.invoke(messages)
+    return {"messages": [response]}
+
+# This is a tool node
+tool_node = ToolNode(tools)
+
+# sqlite database 
+
+conn = sqlite3.connect(database="Rag_Chatbot", check_same_thread=False)
+
+
+# checkpointer
+checkpointer = SqliteSaver(conn = conn)
+
+
+graph = StateGraph(ChatState)
+
+# nodes 
+graph.add_node("chat_node", chat_node)
+graph.add_node("tools", tool_node) 
+
+# add edges
+graph.add_edge(START, "chat_node")
+
+# if the LLM asked for a tool, go to ToolNode, else finish
+graph.add_conditional_edges("chat_node", tools_condition)
+graph.add_edge('tools', 'chat_node')
+graph.add_edge('chat_node', END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
+
+
+# getting all the unique threads 
+
+def retrieve_threads():
+    all_threads = set()
+    
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
+        
+    return list(all_threads)
